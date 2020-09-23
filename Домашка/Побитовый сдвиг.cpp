71c0c42d6e438a66c2c10cc89890e65c8e1b9248
Дмитрий Ткаченко <st086996@student.spbu.ru>
2020-09-24 00:42:00 +0300

    домашнее задание

---
+++ Домашка/Побитовый сдвиг.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char* argv[])
+{
+
+	int a = 0;
+	int b = 1;
+	int c = 0;
+	cin >> a;
+
+	c = ~a;
+	//c = a >> 3; //побитовый сдвиг вправо на 3
+	//c = a << 5; //побитовый сдвиг влево на 5
+
+
+	cin >> a;
+
+
+	return EXIT_SUCCESS;
+}
