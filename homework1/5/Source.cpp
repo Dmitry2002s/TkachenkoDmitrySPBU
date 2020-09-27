86e1aa0c7573f0fbde4379c373d352c2d9440f7a
Дмитрий Ткаченко <st086996@student.spbu.ru>
2020-09-27 13:07:19 +0300

    fixed #5

--- homework1/5/Source.cpp
+++ homework1/5/Source.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<math.h>
 
 using namespace std;
 int main(int argc, char* argv[])
@@ -7,20 +8,29 @@ int main(int argc, char* argv[])
 	int p = 1;
 	int n = 1;
 	int k = 1;
-	int b = 1;
-	int c = 1;
+	int b = 0;
+	int c = 0;
 
 	cin >> p;
-	while (n <= (p/2) )
+	while (n <= sqrt(p) )
 	{
 		k = p % n;
+		c = p / n;
+		if (c == n)
+		{
+			b--;
+		}
 		n++;
 		if (k == 0)
 		{
-			b++;
+			b++;b++;
+			
 		}
 	}
-
+    if(p==1)
+	{
+		b = 1;
+	}
 	cout << b;
 
 
