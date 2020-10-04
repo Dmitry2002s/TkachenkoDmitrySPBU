0c1b06cbb0c8e771ccdc4d886bad46792fcd673d
Дмитрий Ткаченко <st086996@student.spbu.ru>
2020-10-04 12:35:26 +0300

    update 2020 10 04

--- HomeWork2/HomeWork2/Again.cpp
+++ HomeWork2/HomeWork2/Again.cpp
@@ -1,6 +1,5 @@
 ﻿#include<iostream>
 #include<clocale>
-
 using namespace std;
 
 void printMenu()
@@ -12,7 +11,8 @@ void printMenu()
     cout << "3 - Найти номер максимального элемента массива" << endl;
     cout << "4 - Найти минимальный элемент массива" << endl;
     cout << "5 - Посчитать сумму элементов массива" << endl;
-    cout << "6 - Вывести массив в обратном порядке" << endl; 
+    cout << "6 - Вывести массив в обратном порядке" << endl;
+    cout << "7 - Добавить n случайных элементов" << endl;
 }
 
 void expandArray(int*& arr, int& cap)
@@ -32,9 +32,9 @@ void expandArray(int*& arr, int& cap)
 void addElement(int*& arr, int& count, int& cap, int x)
 {
     
-    if (count = cap)
+    if (count == cap)
     {
-        expandArray(*&arr, cap);
+        expandArray(arr, cap);
     }
     arr[count] = x;
     count++; 
@@ -42,20 +42,19 @@ void addElement(int*& arr, int& count, int& cap, int x)
 
 void addRandomElements(int*& arr, int& count, int& cap, int n, int min, int max)
 {
-    //q
+    for (int i = 0; i < n;i++)
+        addElement(arr, count, cap, rand() % (max - min + 1) + min);
 }
 
 void printArray(int* arr, int count,int cap)
 {
-    cout << "[" << count << "/" << cap << "]";
+    cout << "[" << count << "/" << cap << "]" << endl; 
     cout << "{";
     for (int i = 0; i < count;i++)
     {
         cout << arr[i] << (i != count - 1 ? ", " : "");
     }
-    cout << "Введите любую клавишу,чтобы вернуться в главное меню" << endl; 
-        int p = 0;
-        cin >> p;
+    cout << "}" << endl; 
 }
 
 int sumArray(int* arr, int lenght)
@@ -69,8 +68,15 @@ int sumArray(int* arr, int lenght)
     return result;
 }
 
+void inversion(int cap; int* arr)
+{
 
-void processChoice(int*& arr, int& count, int cap, int choice)
+    for (int i = 0;i < (cap / 2); i++)
+        arr[i] = arr[cap - i];
+
+}
+
+void processChoice(int*& arr, int& count, int& cap, int choice)
 {
     switch (choice)
     {
@@ -79,15 +85,41 @@ void processChoice(int*& arr, int& count, int cap, int choice)
         int x = 0;
         cout << "введите значение" << endl; 
         cin >> x; 
-        addElement(*&arr, count, cap, x);
+        addElement(arr, count, cap, x);
     }
         break;
     case 2:
     {
         printArray(arr, count, cap) ;
+        system("pause");
+
     }
-        break;
+    break;
+       
+    case 5:
+    {
+       
+        cout << "Сумма массива равна= " << sumArray(arr, cap) << endl; 
+        system("pause");
     }
+    break;
+    case 7:
+    {
+        int n = 0; 
+        cout << "n=";
+        cin >> n; 
+        int min = 0; 
+        cout << "min=";
+            cin >> min;
+            int max = 0; 
+            cout << "max=";
+            cin >> max; 
+            addRandomElements(arr, count, cap, n, min, max);
+    }
+    break;
+    }
+   
+
 }
 
 
