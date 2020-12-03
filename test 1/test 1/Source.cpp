19efc0e2f25ebd44e219d3b3f04442443d279ba1
Дмитрий Ткаченко <st086996@student.spbu.ru>
2020-12-03 11:08:37 +0300

    test#1

---
+++ test 1/test 1/Source.cpp
@@ -0,0 +1,217 @@
+﻿#include <iostream>
+#include <clocale>
+#include <math.h>
+#include <string>
+
+using namespace std;
+
+void trim(string& str)
+{
+	int SI = 0;
+	while (str[SI] == ' ')
+	{
+		SI++;
+	}
+	str = str.substr(SI);
+}
+double MyFunction(double x, int n)
+{
+	double c = 1; 
+	int p = 0; 
+	double xn = 1; 
+	double f = xn; 
+	p = 1 - x;
+		for (int i = 0; i <= n;++i)
+		{
+			f = f* x; 
+			if (i % 2 == 1) {
+				c = c + f;
+				; 
+			}
+			if (i % 2 == 0)
+			{
+				c = c - f;
+				; 
+			}
+		}
+		
+	return c;
+}
+double MyFunction2(double x, int n)
+{
+	double c = 1;
+	int p = 0;
+	double xn = 1;
+	double f = xn;
+	p = 1 - x;
+	for (int i = 0; i <= n;++i)
+	{
+		f = f * x;
+		c = c + f; 
+	}
+
+	return c;
+}
+int length(string& str)
+{
+	int result = 0;
+	for (int i = 0; str[i] != '\0';i++)
+	{
+		result += 1;
+	}
+	return result;
+}
+int checklength(string& str)
+{
+	if (length(str) > 9)
+	{
+		return 2;
+	}
+	return 0;
+}
+int check(string& str)
+{
+	int p = 0;
+	int s = 0;
+	int m = 0;
+	for (int i = 0; str[i] != '\0';i++)
+	{
+		if (str[i] == '-'|| str[i] == '0' || str[i] == '1' || str[i] == '2' || str[i] == '3' || str[i] == '4' || str[i] == '5' || str[i] == '6' || str[i] == '7' || str[i] == '8' || str[i] == '9' || str[i] == '.')
+		{
+			if (str[i] != '.')
+			{
+				m++;
+			}
+			if (str[i] == '.')
+			{
+				s++;
+				if (s > 1)
+				{
+					return 3;
+				}
+			}
+		}
+		else
+		{
+			p = 1;
+		}
+	}
+	if (str[0] == '.')
+	{
+		return 4;
+	}
+	if (m == 0)
+	{
+		if (s == 1)
+		{
+			return 4;
+		}
+	}
+
+	if (checklength(str) == 2)
+	{
+		return 2;
+	}
+	if (str[0] == 0)
+	{
+		return 5;
+	}
+	return p;
+}
+double StringToDouble(string& str)
+{
+	int k = 0;
+	double  result = 0;
+	int l = 1;
+
+	if (str[0] == '-')
+	{
+		for (int i = 0; str[i] != '\0';i++)
+		{
+			if (str[i] == '.')
+			{
+
+			}
+			if (str[i] == '-')
+			{
+
+			}
+			else result = result * 10 + str[i] - '0';
+		}
+		result = -result;
+	}
+	else
+	{
+		for (int i = 0; str[i] != '\0';i++)
+	{
+		if (str[i] == '.')
+		{
+
+		}
+		else result = result * 10 + str[i] - '0';
+	}
+}
+	return result;
+
+
+}
+int cbc(string str) //ôóíêöèÿ ïîäñ÷¸òà êîë-âà çíàêîâ ïîñëå çàïÿòîé 
+{
+	int result = 0;
+	int k = 0;
+	int i = 0;
+	while (str[i] != '\0')
+	{
+
+		if (str[i] == '.')
+		{
+			k = i;
+		}
+		i++;
+	}
+	if (k == 0)
+	{
+		k = i - 1;
+	}
+	int p = i - 1;
+	result = p - k;
+	return result;
+}
+int znak(int k)
+{
+	int result = k; 
+	int p = 1; 
+	for (int i = 0; i < k;i++)
+	{
+		p *= 10;
+	}
+	return p; 
+}
+int main()
+{
+	string str;
+	int n = 0;
+	getline(cin, str);
+	trim(str);
+	if (check(str) != 0)
+	{
+		cout << "Ошибка" << endl;
+		return 1;
+	}
+	double x = StringToDouble(str) / znak(cbc(str));
+	
+	if (str[0] == '-')
+	{
+		x = x - 2;;
+	}
+	cout << x;
+	double xn = 0;
+
+	cin >> n;
+	double p = 0;
+	
+	cout << MyFunction(x, n) << endl;
+
+
+		;
+}
