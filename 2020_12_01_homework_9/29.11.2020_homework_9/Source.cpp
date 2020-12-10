27a7d7f51d9eda2729e2bcade653561003d20cc8
Дмитрий Ткаченко <st086996@student.spbu.ru>
2020-12-10 15:15:07 +0300

    repair

--- 2020_12_01_homework_9/29.11.2020_homework_9/Source.cpp
+++ 2020_12_01_homework_9/29.11.2020_homework_9/Source.cpp
@@ -92,7 +92,7 @@ int check(string& str)
 	int m = 0; 
 	for (int i = 0; str[i] != '\0';i++)
 	{
-		if (str[i] == '0' || str[i] == '1' || str[i] == '2' || str[i] == '3' || str[i] == '4' || str[i] == '5' || str[i] == '6' || str[i] == '7' || str[i] == '8' || str[i] == '9' || str[i] == '.')
+		if (str[i] >= '0' && str[i] <= '9')
 		{
 			if (str[i] != '.')
 			{
@@ -310,6 +310,6 @@ int main()
 	{
 		cout<<"ïåğåçàïóñê ïğîãğàììû" <<endl; 
 	}
-	exit;
+	;
 	;
 }
