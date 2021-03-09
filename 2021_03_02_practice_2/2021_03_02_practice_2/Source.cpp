6e5633315aff53e24fda3148d132ec4c68018539
Дмитрий Ткаченко <st086996@student.spbu.ru>
2021-03-09 15:20:44 +0300

    the app works with natural and negative numbers

--- 2021_03_02_practice_2/2021_03_02_practice_2/Source.cpp
+++ 2021_03_02_practice_2/2021_03_02_practice_2/Source.cpp
@@ -16,6 +16,8 @@ int index(char i)
 	{
 	default:
 		return k = 0;
+		cout << "lol , you are durak" << endl;
+		break;
 	case '+':
 	{
 		return k = 1;
@@ -40,7 +42,7 @@ int index(char i)
 		return k = 5;
 
 	}
-	cout << "lol , you are durak" << endl;
+
 	break;
 	}
 }
@@ -48,8 +50,13 @@ template <typename T1, typename T2>
 T1 operation(T1 a, T2 b)
 {
 	char i;
-	cin >> i;
+	cout << "select an action (+,-,*,/,%)" << endl;
 
+	cin >> i;
+	cout << "enter a number1 " << endl; 
+	cin >> a; 
+	cout << "enter a number2 " << endl;
+	cin >> b; 
 	int k = index(i);
 
 	cout << "index operation= " << k << endl; ;
@@ -73,12 +80,16 @@ T1 operation(T1 a, T2 b)
 	{
 		return (a * b);
 	}
+	if (k == 0)
+	{
+		cout << "Error - action selected incorrectly" << endl; 
+		return 0; 
+	}
 	return a;
 }
 
 int main()
 {
-		
 	cout << "it is resultat   = " <<operation(954, 3);
 	return 0;
 }
