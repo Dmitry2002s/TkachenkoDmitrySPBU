9a4d8a2deece7836d8f356d3002113b0812b5002
Дмитрий Ткаченко <st086996@student.spbu.ru>
2021-03-14 21:01:12 +0300

    add funciton's array

--- 2021_03_02_practice_2/2021_03_02_practice_2/Source.cpp
+++ 2021_03_02_practice_2/2021_03_02_practice_2/Source.cpp
@@ -5,12 +5,12 @@ int index(char i)
 {
 	int k = 0;
 	switch (i){
-	case '+':return k = 1;
-	case '-':return k = 2;
-	case '/':return k = 3;
-	case '%':return k = 4;
-	case '*':return k = 5;
-	default:return k = 0;
+	case '+':return k = 0;
+	case '-':return k = 1;
+	case '/':return k = 2;
+	case '%':return k = 3;
+	case '*':return k = 4;
+	default:return k = -1;
 	break;
 	}
 }
@@ -47,7 +47,7 @@ T1 summ(T1 a, T2 b)
 	return a + b;
 }
 template <typename T1, typename T2>
-T1 operation(T1 a, T2 b)
+T1 calculate(T1 a, T2 b)
 {
 	char i;
 	cout << "select an action (+,-,*,/,%)" << endl;
@@ -58,38 +58,14 @@ T1 operation(T1 a, T2 b)
 	cin >> b;
 	int k = index(i);
 
-	cout << "index operation= " << k << endl; ;
-	if (k == 1)
-	{
-		return summ(a, b);
-};
-	if (k == 2) 
-	{
-		return diff(a, b);
-	};
-	if (k == 3) 
-	{
-		return div(a, b);
-};
-	if (k == 4) 
-	{ 
-		return rem(a, b);
-	};
-
-	if (k == 5)
-	{
-		return mult(a, b);
-	};
-	if (k == 0)
-	{
-		cout << "Error - action selected incorrectly" << endl;
-	}
-	return 0;
+	cout << "index operation= " << k << endl; 
+	T1(*operation[5])(T1, T2) { summ, diff, div, rem, mult };
+	return operation[index(i)](a, b);
 }
 
 
 int main()
 {
-	cout << "it is resultat   = " << operation(954.0, 3.0);
+	cout << "it is resultat   = " << calculate(954.0, 3.0);
 	return 0;
 }
