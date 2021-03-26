39b8e6775977e77026f14bd7f1a2caa983ea140e
Дмитрий Ткаченко <st086996@student.spbu.ru>
2021-03-26 14:08:24 +0300

    progress fix

--- 2021_03_19_practice_3/2021_03_19_practice_3/Source.cpp
+++ 2021_03_19_practice_3/2021_03_19_practice_3/Source.cpp
@@ -11,7 +11,10 @@ bool isExp(string str, int& index);
 bool isMantis(string str, int& index);
 bool isReal(string str, int& index);
 bool isWhole(string str, int& index);
-
+bool isID(char c);
+bool isletter(char c);
+bool ismultiplier(string str, int& index);
+bool isterm(string str, int& index);
 int main()
 {
 	/*	ifstream fin("in.txt");
@@ -37,23 +40,17 @@ int main()
 	return 0;
 	}*/
 	int index = 0; 
+	int k = 0; 
 	string a; 
 	getline(cin, a);
-	cout << "test = "; 
-	if (isWhole(a, index) == true)
-	{
-		cout << "true" << endl;
-	}
-	else
-	{
-		cout << "Fake" << endl; 
-	}
-	
+	cout << "test Whole= " << isWhole(a, index) << endl; 
+	cout << "test exp= " << isExp(a, index) << endl;
+	cout << "test E" << isE(a, index) << endl; 
 	return 0; 
 }
 bool isSign(char c)
 {
-	return c == '+' || c == '-';
+	return c == '+' || c == '-'||c=='\'' || c=='*' ;
 }
 bool isDigit(char c)
 {
@@ -88,15 +85,7 @@ bool isNatural(string str, int& index)
 }
 bool isExp(string str, int& index)
 {
-	if (!isE(str, index))
-	{
-		return false; 
-	}
-	if (isSign(str[index]))
-	{
-		return true; 
-	}
-	return false; 
+	return isterm(str, index) || (isSign(str[index]));
 }
 bool isReal(string str, int& index)
 {
@@ -118,11 +107,16 @@ bool isMantis(string str, int& index)
 	}
 	return false; 
 }
-bool isWhole(string str, int& index)
+bool isWhole(string str, int& i)
 {
-	for (int i = 0; str[i] != '\0';i++ )
+	if (str[i] == '\0')
 	{
-		
+		return true;
+	}
+		if (isSign(str[i]))
+		{
+			i++;
+		}
 		if (!isDigit(str[i]))
 		{
 			return false;
@@ -131,7 +125,32 @@ bool isWhole(string str, int& index)
 		{
 			return false;
 		}
-		
+		return isWhole(str, ++i );
 	}
-	return true;
+	
+
+bool isID(char c)
+{
+	return isletter(c) || c >= 'a' && c <= 'z';
+}
+bool isletter(char c)
+{
+	return c == 'x' || c == 'z' || c == 'y';
+}
+bool ismultiplier(string str, int& index)
+{
+
+	return isWhole(str, index) || isID(str[index]) || isExp(str, index);
+}
+bool isterm(string str, int& index)
+{
+	if (str[index] == '\0')
+	{
+		return true; 
+	}
+ 
+	if (ismultiplier(str, index) || isSign( str[index]))
+	{
+		return isterm(str, ++index);
+	}
 }
