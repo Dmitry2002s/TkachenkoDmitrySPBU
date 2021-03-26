da6b3ecc566571a6b2981af66e91600b4b969ac8
Дмитрий Ткаченко <st086996@student.spbu.ru>
2021-03-26 18:38:06 +0300

    intermediate success

---
+++ 2021_03_26_Homework_4/2021_03_26_Homework_4/Source.cpp
@@ -0,0 +1,167 @@
+﻿#include <iostream>
+using namespace std; 
+void swap(int& a, int& b);
+void fillArray(int* a, int len);
+void printArray(int* a, int len);
+void mixArray(int* a, int len); 
+bool isSorted(int* a, int len, bool ascending = true);
+void bubbleSort(int* a, int len);
+void insertionSort(int* a, int len);
+void selectionSort(int* a, int len);
+void ascending(int* a, int len);
+void descending(int* a, int len);
+int main()
+{
+	int len = 12; 
+	int* a = new int[len];
+	fillArray(a, len);
+	printArray(a, len);
+	cout << "bubble sort" << endl; 
+	mixArray(a, len);
+	printArray(a, len);
+	bubbleSort(a, len);
+	printArray(a, len);
+	cout << "insertion sort" << endl; 
+	mixArray(a, len);
+	printArray(a, len);
+	insertionSort(a, len);
+	printArray(a, len);
+	cout << "selection sort" << endl; 
+	mixArray(a, len);
+	printArray(a, len);
+	selectionSort(a, len);
+	printArray(a, len);
+	ascending(a, len);
+	cout << "ascending " << endl; 
+	printArray(a, len);
+	descending(a, len);
+	cout << "descending " << endl;
+	printArray(a, len);
+	delete[] a; 
+	return 0; 
+
+}
+void swap(int& a, int& b)
+{
+	int c = a; 
+	a = b; 
+	b = c; 
+}
+void fillArray(int* a, int len)
+{
+	for (int i = 0; i < len;++i)
+	{
+		a[i] = rand() % 90 + 10; 
+	}
+}
+void inputArray(int* a, int len)
+{
+	for (int i = 0;i < len;++i)
+	{
+		cin >> a[i];
+	}
+}
+void printArray(int* a, int len)
+{
+	for (int i = 0; i < len; ++i)
+	{
+		cout << a[i] << ' '; 
+	}
+	cout << endl; 
+}
+void mixArray(int* a, int len)
+{
+	for (int i = 0; i < len; ++i)
+	{
+		swap(a[i], a[rand() % len]);
+	}
+}
+bool isSorted(int* a, int len, bool ascending)
+{
+	for (int i = 0; i < len - 1; ++i)
+	{
+		if (a[i] > a[i + 1] == ascending)
+		{
+			return false; 
+		}
+	}
+	return true; 
+}
+void bubbleSort(int* a, int len)
+{
+	int s = 0; 
+	int k = 0; 
+	for (int i = 0; i < len-1;++i)
+	{
+		for (int j = 0; j < len - i-1;++j)
+		{
+			s++; 
+			if (a[j] > a[j + 1])
+			{
+				swap(a[j], a[j + 1]);
+				k++; 
+			}
+		}
+	}
+	cout << "number of swaps = " << k << endl;
+	cout << "number of comparisons = " << s << endl;
+}
+void insertionSort(int* a, int len)
+{
+	int s = 0;
+	int k = 0;
+	for (int i = 1; i < len; ++i)
+	{
+		int t = a[i];
+		int j = i; 
+		s++; 
+		while (j > 0 && a[j - 1] > t)
+		{
+			k++; 
+			a[j] = a[j - 1];
+			--j; 
+		}
+		k++;
+		a[j] = t;
+	}
+	cout << "number of swaps = " << k << endl;
+	cout << "number of comparisons = " << s << endl;
+
+}
+void selectionSort(int* a, int len)
+{
+	int s = 0; 
+	int k = 0;
+	for (int i = 0; i < len - 1; ++i)
+	{
+		int index = i; 
+		for (int j = i + 1; j < len; ++j)
+		{
+			s++; 
+			if (a[j] < a[index])
+			{
+				index = j;
+			}
+		}
+		k++;
+		swap(a[i], a[index]);
+	}
+	cout << "number of swaps = " << k << endl;
+	cout << "number of comparisons = " << s << endl; 
+ }
+
+void ascending(int* a, int len)
+{
+	for (int i = 0; i < len; ++i)
+	{
+		a[i] = i+1; 
+	}
+}
+
+void descending(int* a, int len)
+{
+	for (int i = 0; i < len; ++i)
+	{
+		a[i] = len - i;
+	}
+}
